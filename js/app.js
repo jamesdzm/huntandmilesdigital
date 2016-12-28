8b5688d85d0579f22b7717d5275e34c99b2e103b
James Dziewulski-Miles <j.dziewulskimiles@gmail.com>
2016-12-28 22:12:26 +0000

    Update

--- js/app.js
+++ js/app.js
@@ -1,48 +1,17 @@
 $(document).ready(function() {
-	$('.masthead-slider').unslider({
-		dots: false,
-		autoplay: true,
-		infinite: true,
-		arrows: false,
-		speed : 500,
-		delay : 5000
+	// jQuery ajax POST for contact form submission
+	$('#contact-form').on('submit', function(e) {
+	  e.preventDefault();
+		$.ajax({
+			url: 'https://formspree.io/enquiries@huntandmilesdigital.co.uk',
+			method: 'POST',
+			data: $('#contact-form').serialize(),
+			dataType: 'json',
+			beforeSend: function() { /* sending message... */ },
+			success: function(data) {
+				location.reload();
+			},
+			error: function(err) { /* error message... */ }
+		});
 	});
-
-	// Hero buttons jQuery smooth scroll
-	$("#learn-more").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#key-points").offset().top
-    }, 1500);
-	});
-
-	$("#contact-us").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#contact-section").offset().top
-    }, 1500);
-	});
-
-	$("#inspired-contact-us").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#contact-section").offset().top
-    }, 1500);
-	});
-
-
-$('#contact-form').on('submit', function(e) {
-  e.preventDefault();
-	$.ajax({
-		url: 'https://formspree.io/enquiries@huntandmilesdigital.co.uk',
-		method: 'POST',
-		data: $('#contact-form').serialize(),
-		dataType: 'json',
-		beforeSend: function() { /* sending message... */ },
-		success: function(data) {
-			location.reload();
-		},
-		error: function(err) { /* error message... */ }
-	});
-});
-
-
-
 });
